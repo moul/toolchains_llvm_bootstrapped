8d02701a645d3c9f3e9b7439f66a27d5e31c9ff9
Corentin Kerisit <corentin.kerisit@gmail.com>
2025-05-27 17:17:36 +0200

    Fix tests on CI

--- tests/e2e.sh
+++ tests/e2e.sh
@@ -1,13 +1,16 @@
-bazel build //tests:main --platforms=//platforms:macos_aarch64
+
+if [ "$GITHUB_ACTIONS" != "true" ]; then
+  bazel build //tests:main --platforms=//platforms:macos_aarch64 --extra_toolchains=//toolchain:all $@
+fi
 
 # Defaults to default glibc
-bazel build //tests:main --platforms=//platforms:linux_x86_64
-bazel build //tests:main --platforms=//platforms:linux_aarch64
+bazel build //tests:main --platforms=//platforms:linux_x86_64 --extra_toolchains=//toolchain:all $@
+bazel build //tests:main --platforms=//platforms:linux_aarch64 --extra_toolchains=//toolchain:all $@
 
 for i in $(seq 28 41); do
-  bazel build //tests:main --platforms=//platforms/libc_aware:linux_x86_64_gnu.2.$i
-  bazel build //tests:main --platforms=//platforms/libc_aware:linux_aarch64_gnu.2.$i
+  bazel build //tests:main --platforms=//platforms/libc_aware:linux_x86_64_gnu.2.$i --extra_toolchains=//toolchain:all $@
+  bazel build //tests:main --platforms=//platforms/libc_aware:linux_aarch64_gnu.2.$i --extra_toolchains=//toolchain:all $@
 done
 
-bazel build //tests:main --platforms=//platforms/libc_aware:linux_x86_64_musl
-bazel build //tests:main --platforms=//platforms/libc_aware:linux_aarch64_musl
+bazel build //tests:main --platforms=//platforms/libc_aware:linux_x86_64_musl --extra_toolchains=//toolchain:all $@
+bazel build //tests:main --platforms=//platforms/libc_aware:linux_aarch64_musl --extra_toolchains=//toolchain:all $@
