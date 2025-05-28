71aa7d3b3842741a7cefec4b84db896a69ea65c8
Corentin Kerisit <corentin.kerisit@gmail.com>
2025-05-28 15:49:45 +0200

    Fix release CI script

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-set -o errexit -o nounset -o pipefail
+set -o errexit -o nounset -o pipefail -x
 
 # Argument provided by reusable workflow caller, see
 # https://github.com/bazel-contrib/.github/blob/d197a6427c5435ac22e56e33340dff912bc9334e/.github/workflows/release_ruleset.yaml#L72
 TAG=$1
 # The prefix is chosen to match what GitHub generates for source archives
-PREFIX="toolchains_llvm_bootstrapped-${TAG:1}"
+PREFIX="toolchains_llvm_bootstrapped-$TAG"
 ARCHIVE="toolchains_llvm_bootstrapped-$TAG.tar.gz"
 ARCHIVE_TMP=$(mktemp)
 
@@ -27,7 +27,7 @@ TODO
 2. Add to your \`MODULE.bazel\` file:
 
 \`\`\`starlark
-bazel_dep(name = "toolchains_llvm_bootstrapped", version = "${TAG:1}")
+bazel_dep(name = "toolchains_llvm_bootstrapped", version = "$TAG")
 
 register_toolchains(
     "@toolchains_llvm_bootstrapped//toolchain:all",
