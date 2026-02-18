395553575e39aac540ec48ecf0dedc7ee25b063a
Fabian Meumertzheim <fabian@meumertzhe.im>
2026-02-18 10:42:51 +0100

    Fix `bazel_dep` snippet in release notes (#310)

--- .github/workflows/release_prep.sh
+++ .github/workflows/release_prep.sh
@@ -5,10 +5,11 @@ set -o errexit -o nounset -o pipefail -x
 # Argument provided by reusable workflow caller, see
 # https://github.com/bazel-contrib/.github/blob/d197a6427c5435ac22e56e33340dff912bc9334e/.github/workflows/release_ruleset.yaml#L72
 TAG=$1
+VERSION=${TAG:1}
 # The prefix is chosen to match what GitHub generates for source archives
 # This guarantees that users can easily switch from a released artifact to a source archive
 # with minimal differences in their code (e.g. strip_prefix remains the same)
-PREFIX="toolchains_llvm_bootstrapped-${TAG:1}"
+PREFIX="toolchains_llvm_bootstrapped-$VERSION"
 ARCHIVE="toolchains_llvm_bootstrapped-$TAG.tar.gz"
 
 # NB: configuration for 'git archive' is in /.gitattributes
@@ -27,7 +28,7 @@ cat << EOF
 ## Add to your \`MODULE.bazel\` file:
 
 \`\`\`starlark
-bazel_dep(name = "toolchains_llvm_bootstrapped", version = "$TAG")
+bazel_dep(name = "toolchains_llvm_bootstrapped", version = "$VERSION")
 
 register_toolchains("@toolchains_llvm_bootstrapped//toolchain:all")
 \`\`\`
